7381e7c8e1e01b4c550a750f5bf4b5dbdad2a770
AugustineGithinji <158051666+AugustineGithinji@users.noreply.github.com>
2026-03-31 17:28:50 +0100

    Update Node and AzureLabTest

--- src/Node.java
+++ src/Node.java
@@ -12,12 +12,16 @@
 // These descriptions are intended to help you understand how the interface
 // will be used. See the RFC for how the protocol works.
 
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 interface NodeInterface {
 
     /* These methods configure your node.
      * They must both be called once after the node has been created but
      * before it is used. */
-    
+
     // Set the name of the node.
     public void setNodeName(String nodeName) throws Exception;
 
@@ -34,7 +38,7 @@ interface NodeInterface {
     // there are no new incoming messages return.
     // If delay is zero then wait for an unlimited amount of time.
     public void handleIncomingMessages(int delay) throws Exception;
-    
+
     // Determines if a node can be contacted and is responding correctly.
     // Handles any messages that have arrived.
     public boolean isActive(String nodeName) throws Exception;
@@ -42,14 +46,14 @@ interface NodeInterface {
     // You need to keep a stack of nodes that are used to relay messages.
     // The base of the stack is the first node to be used as a relay.
     // The first node must relay to the second node and so on.
-    
+
     // Adds a node name to a stack of nodes used to relay all future messages.
     public void pushRelay(String nodeName) throws Exception;
 
     // Pops the top entry from the stack of nodes used for relaying.
     // No effect if the stack is empty
     public void popRelay() throws Exception;
-    
+
 
     /*
      * These methods provide access to the basic functionality of
@@ -59,7 +63,7 @@ interface NodeInterface {
     // Checks if there is an entry in the network with the given key.
     // Handles any messages that have arrived.
     public boolean exists(String key) throws Exception;
-    
+
     // Reads the entry stored in the network for key.
     // If there is a value, return it.
     // If there isn't a value, return null.
@@ -82,24 +86,44 @@ interface NodeInterface {
 // Complete this!
 public class Node implements NodeInterface {
 
+    private String nodeName;
+    private DatagramSocket socket;
+    private Map<String, String> localStore = new HashMap<>();
+
     public void setNodeName(String nodeName) throws Exception {
-	throw new Exception("Not implemented");
+        this.nodeName = nodeName;
     }
 
     public void openPort(int portNumber) throws Exception {
-	throw new Exception("Not implemented");
+        socket = new DatagramSocket(portNumber);
     }
 
     public void handleIncomingMessages(int delay) throws Exception {
-	throw new Exception("Not implemented");
+        if (delay != 0) {
+            socket.setSoTimeout(delay);
+        } else {
+            socket.setSoTimeout(0); // block forever
+        }
+
+        byte[] buffer = new byte[65536];
+        while (true) {
+            try {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                String message = new String(packet.getData(), 0, packet.getLength(), StandardCharsets.UTF_8);
+                processMessage(message, packet.getAddress(), packet.getPort());
+            } catch (java.net.SocketTimeoutException e) {
+                return; // delay has elapsed, return normally
+            }
+        }
     }
-    
+
     public boolean isActive(String nodeName) throws Exception {
-	throw new Exception("Not implemented");
+        throw new Exception("Not implemented");
     }
-    
+
     public void pushRelay(String nodeName) throws Exception {
-	throw new Exception("Not implemented");
+        throw new Exception("Not implemented");
     }
 
     public void popRelay() throws Exception {
@@ -107,18 +131,30 @@ public class Node implements NodeInterface {
     }
 
     public boolean exists(String key) throws Exception {
-	throw new Exception("Not implemented");
+        throw new Exception("Not implemented");
     }
-    
+
     public String read(String key) throws Exception {
-	throw new Exception("Not implemented");
+        return localStore.get(key);
     }
 
     public boolean write(String key, String value) throws Exception {
-	throw new Exception("Not implemented");
+        localStore.put(key, value);
+        return true;
     }
 
     public boolean CAS(String key, String currentValue, String newValue) throws Exception {
-	throw new Exception("Not implemented");
+        throw new Exception("Not implemented");
     }
+
+    private void processMessage(String message, InetAddress senderAddress, int senderPort) throws Exception {
+        // your message parsing logic here
+    }
+
+    private void sendMessage(String message, InetAddress address, int port) throws Exception {
+        byte[] data = message.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+        socket.send(packet);
+    }
+
 }
